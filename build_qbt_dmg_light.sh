a72795abc0e2ffb59215feba2d6b389dc59a4691
blahdy <9033590+blahdy@users.noreply.github.com>
2020-07-23 08:22:01 +0300

    Update build_qbt_dmg_light.sh

--- build_qbt_dmg_light.sh
+++ build_qbt_dmg_light.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e -u -x
+# -e / set -e / set -o errexit - exit immediately if a command exits with a non-zero status
+# -u / set -u / set -o nounset - treat unset variables as an error when substituting
+# -x / set -x / set -o xtrace  - print commands and their arguments as they are executed
+set -o pipefail     # the return value of a pipeline is the status of the last command to exit with a non-zero status
 
 workdir=$(mktemp -d)            # path must be absolute, must be writable for current user
 echo "current working directory: ${workdir}"
@@ -6,7 +10,7 @@ depsdir="${workdir%/}/ext"      # all dependencies will be placed here
 cd ${workdir}
 
 # download Qt from Git repository
-qt_branch=dev                   # Qt version to use, use latest development version
+qt_branch=5.15                  # Qt version to use
 git clone https://code.qt.io/qt/qt5.git
 cd qt5
 git checkout ${qt_branch}
@@ -18,51 +22,37 @@ cd ..
 
 # download and build OpenSSL
 openssl_ver=OpenSSL_1_1_1-stable  # OpenSSL version to use
-curl -L -s https://github.com/openssl/openssl/archive/${openssl_ver}.tar.gz | tar xz
-[[ $? -eq 0 ]] || exit 1
+curl -L https://github.com/openssl/openssl/archive/${openssl_ver}.tar.gz | tar xz
 
 cd openssl-${openssl_ver}
 
-./config zlib --prefix=${depsdir} -mmacosx-version-min=${min_macos_ver}
-[[ $? -eq 0 ]] || exit 1
-make -j4 || exit 1
-make install_sw || exit 1
+./config no-comp no-deprecated no-dynamic-engine no-tests no-zlib --openssldir=/etc/ssl --prefix=${depsdir} -mmacosx-version-min=${min_macos_ver}
+make -j$(sysctl -n hw.ncpu)
+make install_sw
 
 cd ..
 
 # so, Qt dependencies are satisfied now, time to build Qt
 cd qt5
-# apply few my patches to customize build process and decrease build time.
-# these patches are completely optional, but without second one some configure options can't be set,
-# so they must be removed, and this will increase build time
-curl -L -s "https://www.dropbox.com/s/0kp0m0hy9lkxdql/qtbase-dev.patch?dl=1" | patch -p1 -d qtbase
-curl -L -s "https://www.dropbox.com/s/8obu07g04l17m7f/qttools-dev.patch?dl=1" | patch -p1 -d qttools
-curl -L -s "https://bugreports.qt.io/secure/attachment/79380/pb-fix.patch" | patch -p1
 
 qtbuilddir="../build-qt"
 mkdir ${qtbuilddir} && cd ${qtbuilddir}
-# all -skip instructions are completely optional here, and nothing will changed if they are not specified
-# in case of downloading from git repo using my own method, but I left them here for better compatibility
-${workdir}/qt5/configure -prefix "${depsdir}/Qt" -opensource -confirm-license -release -appstore-compliant -c++std c++14 -no-pch -I "${depsdir}/include" -L "${depsdir}/lib" -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d -skip qtcharts -skip qtconnectivity -skip qtdatavis3d -skip qtdeclarative -skip qtdoc -skip qtgamepad -skip qtgraphicaleffects -skip qtimageformats -skip qtlocation -skip qtmultimedia -skip qtnetworkauth -skip qtpurchasing -skip qtquickcontrols -skip qtquickcontrols2 -skip qtremoteobjects -skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech -skip qtvirtualkeyboard -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebglplugin -skip qtwebsockets -skip qtwebview -skip qtwinextras -skip qtx11extras -skip qtxmlpatterns -make libs -no-compile-examples -no-dbus -no-icu -qt-pcre -system-zlib -ssl -openssl-linked -no-cups -qt-libpng -qt-libjpeg -no-feature-testlib -no-feature-sql -no-feature-concurrent
-[[ $? -eq 0 ]] || exit 1
-make -j4 || exit 1
-make install || exit 1
+${workdir}/qt5/configure -prefix "${depsdir}" -opensource -confirm-license -release -appstore-compliant -c++std c++14 -no-pch -I "${depsdir}/include" -L "${depsdir}/lib" -make libs -no-compile-examples -no-dbus -no-icu -qt-pcre -system-zlib -ssl -openssl-linked -no-cups -qt-libpng -qt-libjpeg -no-feature-testlib -no-feature-sql -no-feature-concurrent
+make -j$(sysctl -n hw.ncpu)
+make install
 
 cd ${workdir}
 
 # download and build Boost
-boost_ver=1.69.0                # Boost version to use
+boost_ver=1.73.0                # Boost version to use
 
 boost_ver_u=${boost_ver//./_}
-curl -L -s https://dl.bintray.com/boostorg/release/${boost_ver}/source/boost_${boost_ver_u}.tar.bz2 | tar xj
-[[ $? -eq 0 ]] || exit 1
+curl -L https://dl.bintray.com/boostorg/release/${boost_ver}/source/boost_${boost_ver_u}.tar.bz2 | tar xj
 
 cd boost_${boost_ver_u}
 
 ./bootstrap.sh
-[[ $? -eq 0 ]] || exit 1
 ./b2 --prefix=${depsdir} --with-system variant=release link=static cxxflags="-std=c++14 -mmacosx-version-min=${min_macos_ver}" install
-[[ $? -eq 0 ]] || exit 1
 
 cd ..
 
@@ -74,16 +64,15 @@ if [[ -f "/Applications/CMake.app/Contents/bin/cmake" ]]
 then
   cmake="/Applications/CMake.app/Contents/bin/cmake"
 else
-  curl -L -s https://cmake.org/files/v3.14/cmake-3.14.0-Darwin-x86_64.tar.gz | tar xz
+  cmake_ver=3.18.0
+  curl -L https://github.com/Kitware/CMake/releases/download/v${cmake_ver}/cmake-${cmake_ver}-Darwin-x86_64.tar.gz | tar xz
   cmakedir=$(ls | grep cmake)
   cmake="${workdir}/${cmakedir}/CMake.app/Contents/bin/cmake"
 fi
-[[ -f "${cmake}" ]] || exit 1
 
-lt_branch=RC_1_2                # libtorrent version to use, use latest development version from 1.1.x versions
+lt_branch=RC_1_2                # libtorrent version to use, use latest development version from 1.2.x versions
 
-curl -L -s https://github.com/arvidn/libtorrent/archive/${lt_branch}.tar.gz | tar xz
-[[ $? -eq 0 ]] || exit 1
+curl -L https://github.com/arvidn/libtorrent/archive/${lt_branch}.tar.gz | tar xz
 
 cd libtorrent-${lt_branch}
 # I build static library, something was changed and now linker produce few warnings during qBittorrent building,
@@ -92,20 +81,17 @@ cd libtorrent-${lt_branch}
 curl -L -s "https://www.dropbox.com/s/ym7fegg4f3hwwnt/lt-static-link-warning-fix.patch?dl=1" | patch -p1
 
 mkdir build && cd build
-export CXXFLAGS="-std=c++14 -mmacosx-version-min=${min_macos_ver}"
-${cmake} -DCMAKE_PREFIX_PATH=${depsdir} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=${depsdir} ..
-make VERBOSE=1 -j4 || exit 1
-make install || exit 1
-unset CXXFLAGS
+${cmake} -DCMAKE_PREFIX_PATH=${depsdir} -DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_EXTENSIONS=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET=${min_macos_ver} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -Ddeprecated-functions=OFF -DCMAKE_INSTALL_PREFIX=${depsdir} ..
+make VERBOSE=1 -j$(sysctl -n hw.ncpu)
+make install
 
 cd ../..
 
 # download and build qBittorrent - some magic comes here :)
 # download the sources
-qbt_branch=masters               # qBittorrent version to use, use latest development version
+qbt_branch=master               # qBittorrent version to use, use latest development version
 
-curl -L -s https://github.com/blahdy/qBittorrent/archive/{$qbt_branch}.tar.gz | tar xz
-[[ $? -eq 0 ]] || exit 1
+curl -L https://github.com/blahdy/qBittorrent/archive/{$qbt_branch}.tar.gz | tar xz
 
 cd qBittorrent-${qbt_branch}
 
@@ -125,9 +111,12 @@ curl -O -J -L -s "https://www.dropbox.com/s/apfv9ofhhftderj/conf.pri?dl=1"
 # first patch disables Qt translations deployment, I'll do it later.
 curl -L -s "https://www.dropbox.com/s/d6sdrvz2zpjnywn/qbt-no-predef-qt-stuff.patch?dl=1" | patch -p1
 
-# next part os this script is part from my another script used to build my own projects for macOS.
+# better HiDPI support
+curl -L -s "https://www.dropbox.com/s/2crekp814e5m2vj/hidpi-hacks-new.patch?dl=1" | patch -p1
+
+# next part of this script is part from my another script used to build my own projects for macOS.
 # I was to lazy to rename/remove variables :), so that in slightly different style.
-QT_ROOT="${depsdir}/Qt"
+QT_ROOT="${depsdir}"
 APP_NAME="qBittorrent"
 SRC_PATH="$PWD"
 
@@ -137,8 +126,7 @@ mkdir "$build_dir"
 cd "$build_dir"
 
 $QT_ROOT/bin/qmake -config release -r "$SRC_PATH/qbittorrent.pro"
-make -j4
-[[ $? == 0 ]] || exit 1
+make -j$(sysctl -n hw.ncpu)
 
 # deploy Qt' libraries for an app
 cd src
@@ -175,7 +163,87 @@ else
   hdiutil convert "$APP_NAME.dmg" -format UDBZ -o "$out_file"
 fi
 
-[[ -f "$out_file" ]] || exit 1
+# move created .dmg file to user's Downloads directory, it is writable everywhere
+mv "$out_file" "$HOME/Downloads/"
+
+cd ${workdir}
+
+# download and build qBittorrent - some magic comes here :)
+# download the sources
+qbt_branch=masters               # qBittorrent version to use, use latest development version
+
+curl -L https://github.com/blahdy/qBittorrent/archive/{$qbt_branch}.tar.gz | tar xz
+
+cd qBittorrent-${qbt_branch}
+
+# as so as it is impossible (or pretty hard) to get work autotools build system on macOS,
+# there are only 2 options what to use to build qBittorrent: CMake and qmake.
+# I don't like CMake in general, moreover it is pretty hard to use with Qt, especially from custom path.
+# thereby I decided to use "Qt's native build system" - qmake. it is maybe not so convenient in case of
+# qBittorrent build configuration, but developers envisaged it and created special file for "user configuration".
+# so, download my recommended project configuration (conf.pri)
+curl -O -J -L -s "https://www.dropbox.com/s/apfv9ofhhftderj/conf.pri?dl=1"
+# Note: exactly the same config can be used to build qBittorrent in QtCreator.
+
+# I don't like the way how official qBittorrent app is packaged for macOS, so I do it in my own way.
+# I didn't suggest any patches used there to qBittorrent developers/maintainers, because I'm not Apple
+# developer and strictly don't know the "true" methods, and these changes were made in my own opinion.
+# patches are completely optional.
+# first patch disables Qt translations deployment, I'll do it later.
+curl -L -s "https://www.dropbox.com/s/d6sdrvz2zpjnywn/qbt-no-predef-qt-stuff.patch?dl=1" | patch -p1
+
+# better HiDPI support
+curl -L -s "https://www.dropbox.com/s/2crekp814e5m2vj/hidpi-hacks-new.patch?dl=1" | patch -p1
+
+# next part of this script is part from my another script used to build my own projects for macOS.
+# I was to lazy to rename/remove variables :), so that in slightly different style.
+QT_ROOT="${depsdir}"
+APP_NAME="qBittorrent"
+SRC_PATH="$PWD"
+
+build_dir="$SRC_PATH/../build-qbt"
+rm -rf "$build_dir"
+mkdir "$build_dir"
+cd "$build_dir"
+
+$QT_ROOT/bin/qmake -config release -r "$SRC_PATH/qbittorrent.pro"
+make -j$(sysctl -n hw.ncpu)
+
+# deploy Qt' libraries for an app
+cd src
+mv qbittorrent.app "$APP_NAME.app"
+$QT_ROOT/bin/macdeployqt "$APP_NAME.app"
+
+# deploy Qt' translations
+tr_path="$PWD/$APP_NAME.app/Contents/Resources/translations"
+[[ -d "$tr_path" ]] || mkdir "$tr_path"
+pushd "$QT_ROOT/translations" > /dev/null
+langs=$(ls -1 qt_*.qm | grep -v help | sed 's/qt_\(.*\)\.qm/\1/g')
+for lang in $langs
+do
+  lang_files=$(ls -1 qt*_$lang.qm)
+  $QT_ROOT/bin/lconvert -o "$tr_path/qt_$lang.qm" $lang_files
+done
+popd > /dev/null
+
+# update generated qt.conf
+echo "Translations = Resources/translations" >> "$APP_NAME.app/Contents/Resources/qt.conf"
+
+# create .dmg file, there magic becomes :)
+codesign --deep --force --verify --verbose --sign "-" "$APP_NAME.app"
+out_file="$build_dir/../qbittorrent-${qbt_branch}-macosx.dmg"
+if [[ $(which dmgbuild) ]]
+then
+  # use 'dmgbuild' utility: https://pypi.org/project/dmgbuild/
+  curl -O -J -L -s "https://www.dropbox.com/s/q315bjd96umlxm0/settings.py?dl=1"
+  dmgbuild -s "settings.py" -D app="$APP_NAME.app" "$APP_NAME" "$out_file"
+else
+  # use 'hdiutil' available on each macOS
+  hdiutil create -srcfolder "$APP_NAME.app" -nospotlight -layout NONE -fs HFS+ "$APP_NAME.dmg"
+  # this is very strange, but much better compression is achieved only after image conversion ...
+  hdiutil convert "$APP_NAME.dmg" -format UDBZ -o "$out_file"
+fi
+
 # move created .dmg file to user's Downloads directory, it is writable everywhere
 mv "$out_file" "$HOME/Downloads/"
 
